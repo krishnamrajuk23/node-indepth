e442edd1d457263d6b3be69cd28ca0c1be33579e
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2022-12-31 00:41:34 +0530

    added cors to allow all can access publicly

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './router';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
 /* Middleware */
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
