93b467af2190393f8b4b504984a8d643e32ce289
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2022-12-31 10:34:38 +0530

    authentication added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config(); // app can able to  access config of env properties
+
 import app from './server';
 
 const port = 3001;
---
+++ src/modules/auth.ts
@@ -0,0 +1,5 @@
+import jwt from 'jsonwebtoken';
+
+export const createJWT = (user: any) => {
+  const token = jwt.sign({id: user.id, username: user.username}, process.env.JWT_SECRET as string);
+}
